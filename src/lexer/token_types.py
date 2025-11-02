4709e039a4a981aaaa60b9aab40e195be0973c10
franz . <fssaragena@up.edu.ph>
2025-11-03 00:32:37 +0800

    added: the remaining regex keywords

--- src/lexer/token_types.py
+++ src/lexer/token_types.py
@@ -1,3 +1,8 @@
+##################
+# TOKENS
+#################
+
+
 MULTI_KEYWORDS = [
     # variable declaration
     ("I HAS A", "Variable Declaration"),
@@ -48,13 +53,72 @@ MULTI_KEYWORDS = [
 ]
 
 REGEX_KEYWORDS = [
-    # liteals
+    # literals
     (r"^\-?[0-9]+\.[0-9]+", "Float Literal"),
     (r"^\-?[0-9]+", "Integer Literal"),
+    (r'^".*"', "Yarn Literal"),
 
     # troof
     (r"^WIN", "Boolean Value (True)"),
     (r"^FAIL", "Boolean Value (False)"),
 
-    # CUT MUNA DITO MAGPAPARES MUNA KO
+    # type literal
+    (r"^(NUMBR|NUMBAR|YARN|TROOF)$", "Type Literal"),
+
+    # other keywords (di ko alam if need tanggalin yung $ depende guro sa pag implement natin sa interpreter )
+    (r"^HAI$", "HAI"),
+    (r"^KTHXBYE$", "KTHXBYE"),
+    (r"^WAZZUP$", "WAZZUP"),
+    (r"^BUHBYE$", "BUHBYE"),
+    (r"^BTW$", "BTW"),
+    (r"^OBTW$", "OBTW"),
+    (r"^TLDR$", "TLDR"),
+    (r"^I HAS A$", "I_HAS_A"),
+    (r"^ITZ$", "ITZ"),
+    (r"^R$", "R"),
+    (r"^SUM OF$", "SUM_OF"),
+    (r"^DIFF OF$", "DIFF_OF"),
+    (r"^PRODUKT OF$", "PRODUKT_OF"),
+    (r"^QUOSHUNT OF$", "QUOSHUNT_OF"),
+    (r"^MOD OF$", "MOD_OF"),
+    (r"^BIGGR OF$", "BIGGR_OF"),
+    (r"^SMALLR OF$", "SMALLR_OF"),
+    (r"^BOTH OF$", "BOTH_OF"),
+    (r"^EITHER OF$", "EITHER_OF"),
+    (r"^WON OF$", "WON_OF"),
+    (r"^NOT$", "NOT"),
+    (r"^ANY OF$", "ANY_OF"),
+    (r"^ALL OF$", "ALL_OF"),
+    (r"^BOTH SAEM$", "BOTH_SAEM"),
+    (r"^DIFFRINT$", "DIFFRINT"),
+    (r"^SMOOSH$", "SMOOSH"),
+    (r"^MAEK$", "MAEK"),
+    (r"^A$", "A"),
+    (r"^IS NOW A$", "IS_NOW_A"),
+    (r"^VISIBLE$", "VISIBLE"),
+    (r"^GIMMEH$", "GIMMEH"),
+    (r"^O RLY\?$", "O_RLY"),
+    (r"^YA RLY$", "YA_RLY"),
+    (r"^MEBBE$", "MEBBE"),
+    (r"^NO WAI$", "NO_WAI"),
+    (r"^OIC$", "OIC"),
+    (r"^WTF\?$", "WTF"),
+    (r"^OMG$", "OMG"),
+    (r"^OMGWTF$", "OMGWTF"),
+    (r"^IM IN YR$", "IM_IN_YR"),
+    (r"^UPPIN$", "UPPIN"),
+    (r"^NERFIN$", "NERFIN"),
+    (r"^YR$", "YR"),
+    (r"^TIL$", "TIL"),
+    (r"^WILE$", "WILE"),
+    (r"^IM OUTTA YR$", "IM_OUTTA_YR"),
+    (r"^HOW IZ I$", "HOW_IZ_I"),
+    (r"^IF U SAY SO$", "IF_U_SAY_SO"),
+    (r"^GTFO$", "GTFO"),
+    (r"^FOUND YR$", "FOUND_YR"),
+    (r"^I IZ$", "I_IZ"),
+    (r"^MKAY$", "MKAY"),
+
+
 ]
+
